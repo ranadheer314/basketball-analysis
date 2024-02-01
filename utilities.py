8cdcfb7f03e22030ad228ec05f6026b802f03d7f
Ranadheer <ranadheer314@gmail.com>
2024-02-01 00:50:43 -0500

    added functions in utilities

--- utilities.py
+++ utilities.py
@@ -1,5 +1,6 @@
 import requests
 import vault
+import pandas as pd
 
 def get_response(url,query):
     #url = "https://api-nba-v1.p.rapidapi.com/seasons"
@@ -19,3 +20,20 @@ def get_teams(response):
         home=response.json()['response'][i]['teams']['home']['name']
         list_1.append([visitors,home])
     return list_1
+
+
+
+def create_teams(df):
+    
+    team_columns=df['teams'][2]['visitors'].keys()
+    team_df=pd.DataFrame(columns=team_columns)
+    
+    for match in df["teams"]:
+        
+        if match['visitors']['id'] not in team_df['id'].values:
+            team_df=pd.concat([team_df,pd.DataFrame([match['visitors']])])
+            
+        if match['home']['id'] not in team_df['id'].values:
+            team_df=pd.concat([team_df,pd.DataFrame([match['home']])])
+
+    return team_df
